c2a852a26c86bad12ef4461f9a29cd4e3f0b82ef
AlixiaDae <lizziejones1208@gmail.com>
2022-11-16 04:36:09 +0800

    Initial commit with new class lesson

--- script.js
+++ script.js
@@ -1,121 +1,144 @@
-let myLibrary = []
+// let myLibrary = []
 
-const hungerGames = new Book('Hunger Games', 'Suzanne Collins', 384, 'read')
-const songOfAchilles = new Book('Song of Achilles', 'Madeline Miller', 416, 'read')
+// const hungerGames = new Book('Hunger Games', 'Suzanne Collins', 384, 'read')
+// const songOfAchilles = new Book('Song of Achilles', 'Madeline Miller', 416, 'read')
 
-myLibrary.push(hungerGames)
-myLibrary.push(songOfAchilles)
+// myLibrary.push(hungerGames)
+// myLibrary.push(songOfAchilles)
 
-function Book(title, author, pages, status) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.status = status
-}
+// function Book(title, author, pages, status) {
+//     this.title = title
+//     this.author = author
+//     this.pages = pages
+//     this.status = status
+// }
 
-Book.prototype.addtoLibraryArray = (book) => {
-    const existingTitles = myLibrary.map((addedBook) => addedBook.title)
-    if(!existingTitles.includes(book.title)) {
-        myLibrary.push(book)
-    }
-}
-
-
-const shelf = document.querySelector('.shelf')
-
-function showBooks() {
-    shelf.innerHTML = ''
-    for (let book of myLibrary) {
-        createBookCard(book)
-    }
-}
+// Book.prototype.addtoLibraryArray = (book) => {
+//     const existingTitles = myLibrary.map((addedBook) => addedBook.title)
+//     if(!existingTitles.includes(book.title)) {
+//         myLibrary.push(book)
+//     }
+// }
 
 
-const createBookCard = (book) => {
-    const bookCard = document.createElement('div')
-    const title = document.createElement('p')
-    const author = document.createElement('p')
-    const pages = document.createElement('p')
-    const status = document.createElement('div')
-    const removeBtn = document.createElement('button')
+// const shelf = document.querySelector('.shelf')
 
-    if(book.status == 'read') {
-        status.classList.add('green')
-    } else if(book.status == 'inprogress') {
-        status.classList.add('orange')
-    } else {
-        status.classList.add('red')
-    }
+// function showBooks() {
+//     shelf.innerHTML = ''
+//     for (let book of myLibrary) {
+//         createBookCard(book)
+//     }
+// }
 
-    bookCard.classList.add('card')
-    bookCard.setAttribute('data-attribute', `${book.title}`)
-    removeBtn.classList.add('remove')
+
+// const createBookCard = (book) => {
+//     const bookCard = document.createElement('div')
+//     const title = document.createElement('p')
+//     const author = document.createElement('p')
+//     const pages = document.createElement('p')
+//     const status = document.createElement('div')
+//     const removeBtn = document.createElement('button')
+
+//     if(book.status == 'read') {
+//         status.classList.add('green')
+//     } else if(book.status == 'inprogress') {
+//         status.classList.add('orange')
+//     } else {
+//         status.classList.add('red')
+//     }
+
+//     bookCard.classList.add('card')
+//     bookCard.setAttribute('data-attribute', `${book.title}`)
+//     removeBtn.classList.add('remove')
     
 
-    title.textContent = book.title
-    author.textContent = book.author
-    pages.textContent = book.pages
-    removeBtn.textContent = 'X'
+//     title.textContent = book.title
+//     author.textContent = book.author
+//     pages.textContent = book.pages
+//     removeBtn.textContent = 'X'
 
 
-    bookCard.appendChild(title)
-    bookCard.appendChild(author)
-    bookCard.appendChild(pages)
-    bookCard.appendChild(status)
-    bookCard.appendChild(removeBtn)
-    shelf.appendChild(bookCard)
+//     bookCard.appendChild(title)
+//     bookCard.appendChild(author)
+//     bookCard.appendChild(pages)
+//     bookCard.appendChild(status)
+//     bookCard.appendChild(removeBtn)
+//     shelf.appendChild(bookCard)
 
-    status.addEventListener('click', () => {
-        if (status.className == 'green') {
-            status.className = 'red'
-            book.status = 'unread'
-        } else  if(status.className == 'red') {
-            status.className = 'orange'
-            book.status = 'inprogress'
-        } else if(status.className = 'orange') {
-            status.className = 'green'
-            book.status = 'read'
-        }
-    })
+//     status.addEventListener('click', () => {
+//         if (status.className == 'green') {
+//             status.className = 'red'
+//             book.status = 'unread'
+//         } else  if(status.className == 'red') {
+//             status.className = 'orange'
+//             book.status = 'inprogress'
+//         } else if(status.className = 'orange') {
+//             status.className = 'green'
+//             book.status = 'read'
+//         }
+//     })
 
-    removeBtn.addEventListener('click', (btn) => {
-        if (bookCard.dataset.attribute == `${book.title}`) {
-            bookCard.remove()
-            myLibrary.splice(book,1)
-        }
-    })
-}
+//     removeBtn.addEventListener('click', (btn) => {
+//         if (bookCard.dataset.attribute == `${book.title}`) {
+//             bookCard.remove()
+//             myLibrary.splice(book,1)
+//         }
+//     })
+// }
 
 
-const bookForm = document.querySelector('form')
+// const bookForm = document.querySelector('form')
 
-function addBookToLibrary() {
-    const title = document.getElementById('title').value 
-    const author = document.getElementById('author').value 
-    const pages = document.getElementById('pages').value 
-    const status = document.getElementById('status').value 
-    return new Book(title,author,pages,status)
-}
+// function addBookToLibrary() {
+//     const title = document.getElementById('title').value 
+//     const author = document.getElementById('author').value 
+//     const pages = document.getElementById('pages').value 
+//     const status = document.getElementById('status').value 
+//     return new Book(title,author,pages,status)
+// }
 
-const submitBtn = document.getElementById('submit')
+// const submitBtn = document.getElementById('submit')
 
-submitBtn.addEventListener('click' , (input) => {
-    input.preventDefault()
-    const newBook = addBookToLibrary()
-    newBook.addtoLibraryArray(newBook)
-    showBooks()
-})
+// submitBtn.addEventListener('click' , (input) => {
+//     input.preventDefault()
+//     const newBook = addBookToLibrary()
+//     newBook.addtoLibraryArray(newBook)
+//     showBooks()
+// })
 
-const addBookBtn = document.querySelector('.add-book')
-const form = document.querySelector('form')
+// const addBookBtn = document.querySelector('.add-book')
+// const form = document.querySelector('form')
 
-addBookBtn.addEventListener('click', (btn) => {
-    if(form.style.opacity !== '1') {
-        form.style.opacity = '1'
-    } else {
-        form.style.opacity = '0'
-    }
-})
+// addBookBtn.addEventListener('click', (btn) => {
+//     if(form.style.opacity !== '1') {
+//         form.style.opacity = '1'
+//     } else {
+//         form.style.opacity = '0'
+//     }
+// })
     
 
-showBooks()
+// showBooks()
+
+
+class Library {
+    constructor(book) {
+        this.bookName = book;
+    }
+
+
+
+}
+
+const addBookBtn = document.querySelector('.add-book');
+const bookForm = document.querySelector('form');
+
+function showForm() {
+    if(bookForm.style.opacity == 0) {
+    bookForm.style.opacity = '1';
+    } else {
+        return;
+    }
+}
+
+addBookBtn.addEventListener('click', showForm)
